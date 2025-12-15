1a5f02dc45b46a5bfbeb9c9387ea1bf1c21216e0
Yuzora <Yuzora@DESKTOP-7IE92CG>
2025-12-15 17:48:16 +0800

    Add project files.

---
+++ Gibberish/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Gibberish
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            txtUsername = new TextBox();
+            txtPassword = new TextBox();
+            pictureBox1 = new PictureBox();
+            btnLogin = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Info;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 3);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(429, 100);
+            panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Showcard Gothic", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(130, 36);
+            label1.Name = "label1";
+            label1.Size = new Size(189, 33);
+            label1.TabIndex = 0;
+            label1.Text = "Admin Login";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Showcard Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(155, 122);
+            label2.Name = "label2";
+            label2.Size = new Size(128, 27);
+            label2.TabIndex = 1;
+            label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Showcard Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(155, 155);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 27);
+            label3.TabIndex = 2;
+            label3.Text = "Password";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Showcard Gothic", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(38, 235);
+            label4.Name = "label4";
+            label4.Size = new Size(79, 27);
+            label4.TabIndex = 3;
+            label4.Text = "Admin";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(289, 126);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(112, 23);
+            txtUsername.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(289, 155);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(112, 23);
+            txtPassword.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = Properties.Resources.download__7_;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(27, 122);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(105, 110);
+            pictureBox1.TabIndex = 6;
+            pictureBox1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = SystemColors.Info;
+            btnLogin.Location = new Point(299, 209);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(75, 23);
+            btnLogin.TabIndex = 7;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(453, 279);
+            Controls.Add(btnLogin);
+            Controls.Add(pictureBox1);
+            Controls.Add(txtPassword);
+            Controls.Add(txtUsername);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private TextBox txtUsername;
+        private TextBox txtPassword;
+        private PictureBox pictureBox1;
+        private Button btnLogin;
+    }
+}
---
+++ Gibberish/Form1.cs
@@ -0,0 +1,10 @@
+namespace Gibberish
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
